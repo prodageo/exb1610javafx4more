cb09c51df113442529d104613640b9688f877bd4
frederic-baucher <frederic.baucher@insa-rouen.fr>
2021-05-01 20:39:34 +0200

    Update TxnScript.java

--- src/main/java/ch/makery/address/persistance/TxnScript.java
+++ src/main/java/ch/makery/address/persistance/TxnScript.java
@@ -2,5 +2,24 @@ package ch.makery.address.persistance ;
 
 public class TxnScript
 {
+
+	    /* example of call 
+	VilleIterator myVilleIt = myTxnScript.getVilleIterator()
+	int myCurrentCodePostal ;
+	String myCurrentNom ;    
+	while ( myVilleIt.hasNext() )
+	{
+		myCurrentNom = myVilleIt.nextNomVille ( ) ;
+		myCurrentCodePostal = myVilleIt.nextCodePostalVille ( ) ;		
+	        villeData.add(new Ville( myCurrentCodePostal, myCurrentNom));
+	}
+	*/
+  
+  public class VilleIterator
+  {
+    // https://www.baeldung.com/java-nested-classes#static-nested-classes
+    
+
+  }
   
 }
